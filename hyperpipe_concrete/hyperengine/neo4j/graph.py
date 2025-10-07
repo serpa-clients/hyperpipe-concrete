e48fe6b183f6cd23167bc7a3f2a7a37fe1da4cfa
joangq <jgquiroga@dc.uba.ar>
2025-10-07 12:21:42 -0300

    Actualizo graph

--- hyperpipe_concrete/hyperengine/neo4j/graph.py
+++ hyperpipe_concrete/hyperengine/neo4j/graph.py
@@ -1,22 +1,69 @@
 from ..utils import TODO
+import neo4j
 
 type SchemaSummary = None
-type ConfirmationMessage = str
+type ConfirmationMessage = str    
+
+def transaction(tx: neo4j.Transaction, query: str, params: dict = {}) -> list[dict]:
+    data = tx.run(query, **params)
+    return [r.data() for r in data]
 
 class neo4jGraph:
-    async def read_query(
-        self,
-        query: str,
-        params: None | dict
-    ) -> list[dict]:
-        TODO()
+    driver: neo4j.Driver
 
-    async def write_query(
-        self,
-        query: str,
-        params: None | dict
+    def __init__(
+        self, 
+        uri: str = None, 
+        user: str = None, 
+        password: str = None, 
+        database: str = None
+    ):
+        self.driver = self.initialize_driver(
+            uri=uri, 
+            user=user, 
+            password=password
+        )
+        
+        self.database = database
+    
+    def initialize_driver(
+        self, 
+        uri: str, 
+        user: str, 
+        password: str
+    ):
+        driver = neo4j.GraphDatabase.driver(uri, auth=(user, password))
+        try:
+            driver.verify_connectivity()
+        except neo4j.exceptions.ServiceUnavailable:
+            raise ValueError("Could not connect to Neo4j database. Please ensure that the url is correct")
+        except neo4j.exceptions.AuthError:
+            raise ValueError("Could not connect to Neo4j database. Please ensure that the username and password are correct")
+        return driver
+
+    def read_query(
+        self, 
+        query: str, 
+        params: dict = {}
     ) -> list[dict]:
-        TODO()
+        with self.driver.session(database=self.database) as session:
+            try:
+                data = session.execute_read(transaction, query, params)
+                return data
+            except neo4j.exceptions.CypherSyntaxError as e:
+                raise ValueError(f"Generated Cypher Statement is not valid\n{e}")
+
+    def write_query(
+        self, 
+        query: str, 
+        params: dict = {}
+    ) -> list[dict]:
+        with self.driver.session(database=self.database) as session:
+            try:
+                data = session.execute_write(transaction, query, params)
+                return data
+            except neo4j.exceptions.CypherSyntaxError as e:
+                raise ValueError(f"Generated Cypher Statement is not valid\n{e}")
 
     async def schema() -> SchemaSummary:
         TODO()
