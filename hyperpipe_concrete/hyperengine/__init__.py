ea063cafa2f160cee03737725b8b3a1f57b95dc6
Luciano Levy <lucianolevy03@gmail.com>
2025-10-14 11:52:34 -0300

    removed hyperengine and added requirements

--- hyperpipe_concrete/hyperengine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .llm import Llm
-from .embedder import Embedder
-from .neo4j import neo4jGraph
-from .models import Origin, qChunk, qTracker
-from .logger import Logger
--- hyperpipe_concrete/hyperengine/utils.py
+++
@@ -1,2 +0,0 @@
-def TODO():
-    raise NotImplementedError("Not implemented")
